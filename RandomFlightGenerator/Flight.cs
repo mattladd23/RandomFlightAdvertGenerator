2ea535f95aff3fe6f075872a46db40aa2d8ea860
Matt <matt-jbl@hotmail.co.uk>
2022-11-19 14:04:16 +0000

    Add project files.

---
+++ RandomFlightGenerator/Flight.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomFlightGenerator
+{
+    internal class Flight
+    {
+        // Generate random indicies in follow arrays
+        public static Random Randomizer = new Random();
+
+        // Array of departure airports 
+        public string[] DepartureAirports = { "London Gatwick", "London Stansted", "London Luton", "London Heathrow", "London City" };
+
+        // Array of destinations
+        public string[] DestinationAirports = { "Paris", "Berlin", "Madrid", "New York", "Istanbul", "Nairobi", "Singapore" };
+
+        // Array of seat classes
+        public string[] Seats = { "First Class", "Business Class", "Economy Class" };
+
+        // Empty string to populate using generate method
+        public string Description = "";
+
+        // Initialize arbitrary price
+        public string Price;
+
+        public void Generate()
+        {
+            // Variable to randomly select departure airport
+            string randomDeparture = DepartureAirports[Randomizer.Next(DepartureAirports.Length)];
+
+            // Variable to randomly select destination airport
+            string randomDestination = DestinationAirports[Randomizer.Next(DestinationAirports.Length)];
+
+            // Variable to randomly select seat class
+            string randomSeat = Seats[Randomizer.Next(Seats.Length)];
+
+            // Join random variables to produce random flight
+            Description = randomDeparture + " to " + randomDestination + " in " + randomSeat;
+
+
+            // Random price (pounds)
+            decimal pounds = Randomizer.Next(1, 100);
+
+            // Random price (pence)
+            decimal pence = Randomizer.Next(1, 100);
+
+            // Join pounds and pence to form final random price
+            decimal price = pounds + (pence * .01M);
+
+            // Convert price to string and format to local currency
+            Price = "Now only " + price.ToString("c") + "!";
+        }
+    }
+}
---
+++ RandomFlightGenerator/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomFlightGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeFlight();
+        }
+
+        private void MakeFlight()
+        {
+            // Declare new array for flights
+            Flight[] flights = new Flight[6];         
+
+            for (int i = 0; i < flights.Length; i++)
+            {
+                // Instantiate flight for each indicies
+                flights[i] = new Flight();
+
+                // Include condition to have lower flights as domestic flights
+                if (i >= 3)
+                {
+                    flights[i].DestinationAirports = new string[]
+                    {
+                        "Manchester", "Newcastle", "Bristol", "Glasgow", "Belfast"
+                    };
+                }
+
+                // Invoke generate method from flight class
+                flights[i].Generate();
+            }
+
+            // Allocate flight descriptions and prices
+            flight1.Text = flights[0].Description;
+            price1.Text = flights[0].Price;
+
+            flight2.Text = flights[1].Description;
+            price2.Text = flights[1].Price;
+
+            flight3.Text = flights[2].Description;
+            price3.Text = flights[2].Price;
+
+            flight4.Text = flights[3].Description;
+            price4.Text = flights[3].Price;
+
+            flight5.Text = flights[4].Description;
+            price5.Text = flights[4].Price;
+
+            
+            // Create special flight for bottom flight
+            Flight specialFlight = new Flight()
+            {
+                DepartureAirports = new string[] { "London Southend", "Norwich" },
+                DestinationAirports = new string[]
+                {
+                    "Malaga", "Amsterdam", "Exeter"
+                }
+            };
+            specialFlight.Generate();
+
+            // Allocate final flight instance to special flight fields
+            flight6.Text = specialFlight.Description;
+            price6.Text = specialFlight.Price;
+        }
+    }    
+}
